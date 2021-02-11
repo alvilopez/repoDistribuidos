2d2e983176410aa615e98d01a3c03d66f652bb2d
Hubalius2 <pabmarsan2@gmail.com>
2021-02-11 11:09:01 +0100

    Crear Proyecto. Fase 1

---
+++ Dad_Vuelanding/src/main/java/dad/vuelanding/DadVuelandingApplication.java
@@ -0,0 +1,13 @@
+package dad.vuelanding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DadVuelandingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DadVuelandingApplication.class, args);
+	}
+
+}
---
+++ Dad_Vuelanding/src/test/java/dad/vuelanding/DadVuelandingApplicationTests.java
@@ -0,0 +1,13 @@
+package dad.vuelanding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DadVuelandingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
