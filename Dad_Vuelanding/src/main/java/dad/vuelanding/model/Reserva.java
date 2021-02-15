f4827845940b159fb545baf9d922730dc0fcb407
Hubalius2 <pabmarsan2@gmail.com>
2021-02-15 18:45:47 +0100

    Crear Entidades Hotel, Reserva y Usuario

---
+++ Dad_Vuelanding/src/main/java/dad/vuelanding/model/Reserva.java
@@ -0,0 +1,44 @@
+package dad.vuelanding.model;
+
+public class Reserva {
+	
+	/*private Usuario cliente;
+	private Vuelo ida;
+	private Vuelo vuelta;
+	private Hotel hotel;
+	
+	
+	public reserva(Usuario cliente, Vuelo ida, Vuelo vuelta, Hotel hotel) {
+		this.cliente = cliente;
+		this.ida = ida;
+		this.vuelta = vuelta;
+		this.hotel = hotel;
+	}
+	
+	public Usuario getCliente() {
+		return cliente;
+	}
+	public void setCliente(Usuario cliente) {
+		this.cliente = cliente;
+	}
+	public Vuelo getIda() {
+		return ida;
+	}
+	public void setIda(Vuelo ida) {
+		this.ida = ida;
+	}
+	public Vuelo getVuelta() {
+		return vuelta;
+	}
+	public void setVuelta(Vuelo vuelta) {
+		this.vuelta = vuelta;
+	}
+	public Hotel getHotel() {
+		return hotel;
+	}
+	public void setHotel(Hotel hotel) {
+		this.hotel = hotel;
+	}*/
+	
+	
+}
