59776336eedb0952fb5e4e033a928bfced703e3f
alvilopez <alvi_lopez@usal.es>
2021-06-17 17:48:28 +0200

    Hazelcast y corrección de Haproxy

--- Dad_Vuelanding/src/main/java/dad/vuelanding/DadVuelandingApplication.java
+++ Dad_Vuelanding/src/main/java/dad/vuelanding/DadVuelandingApplication.java
@@ -1,14 +1,45 @@
 package dad.vuelanding;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.session.hazelcast.config.annotation.web.http.EnableHazelcastHttpSession;
 
+import com.hazelcast.config.Config;
+import com.hazelcast.config.JoinConfig;
+
+@EnableCaching
 @SpringBootApplication
+@EnableHazelcastHttpSession
 public class DadVuelandingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DadVuelandingApplication.class, args);
 	}
 
+	@Bean
+	public CacheManager cacheManager() {
+		return new ConcurrentMapCacheManager("aeropuertos");
+	}
 	
+	@Bean
+	 public Config config() {
+	 Config config = new Config();
+	 JoinConfig joinConfig = config.getNetworkConfig().getJoin();
+	 joinConfig.getMulticastConfig().setEnabled(false);
+	 
+	 List<String> serversList = new ArrayList<String>();
+     serversList.add("vuelanding");
+     serversList.add("vuelanding2");
+	 
+	 joinConfig.getTcpIpConfig().setEnabled(true).setMembers(serversList);
+	 return config;
+	}
 }
